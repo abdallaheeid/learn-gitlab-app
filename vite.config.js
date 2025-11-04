14ae090c17a6f2608bc788c8f1f4750a2480afa7
Abdallah Eid <abdallaheid01@gmail.com>
2025-11-04 18:00:43 +0000

    Update 2 files
    - /vite.config.js
    - /src/App.jsx

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build-tmp'
+    outDir: 'build'
   },
   test: {
     environment: 'jsdom',
